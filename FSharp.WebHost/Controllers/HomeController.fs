d4f08f40ed1d3a058836fb10d275e56ddb5d8988
Maciek <MagicFleas@bitbucket.org>
2017-07-02 15:44:26 +0200

    removing contact tab

--- FSharp.WebHost/Controllers/HomeController.fs
+++ FSharp.WebHost/Controllers/HomeController.fs
@@ -16,9 +16,5 @@ type HomeController () =
         this.ViewData.["Message"] <- "Your application description page."
         this.View()
 
-    member this.Contact () =
-        this.ViewData.["Message"] <- "Your contact page."
-        this.View()
-
     member this.Error () =
         this.View();
